6c8be81456ba6e86630bf2f03d12753beeb5b0c2
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-08-06 10:40:27 +0530

    created the server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log("working...");
+const express = require("express");
+
+const app = express();
+const port = 9999;
+
+app.listen(9999, () => {
+  console.log("server running on: " + port);
+});
