a8c61ae91651e3b2ed88902b3909b5a068470b6d
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-08-06 12:23:23 +0530

    Created basic api methods

--- src/app.js
+++ src/app.js
@@ -3,6 +3,26 @@ const express = require("express");
 const app = express();
 const port = 9999;
 
+// get
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Arun", lastName: "Kumar" });
+});
+
+//post
+app.post("/user", (req, res) => {
+  res.send("Data saved successfully in database");
+});
+
+//patch
+app.patch("/user", (req, res) => {
+  res.send("Data updated successfully");
+});
+
+//delete
+app.delete("/user", (req, res) => {
+  res.send("user deleted successfully");
+});
+
 app.listen(9999, () => {
-  console.log("server running on: " + port);
+  console.log("server running on port number : " + port);
 });
