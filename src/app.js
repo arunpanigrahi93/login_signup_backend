2a1799b90cdaf7ed1fec6ebb1951b9fa18de15d1
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-08-07 15:13:53 +0530

    validation

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const userAuth = require("./middleware/auth");
 const connectDb = require("./config/datbase");
 const User = require("./model/user");
+const validateSignUpData = require("./helper/validation");
+const bcrypt = require("bcrypt");
 
 const app = express();
 const port = 9999;
@@ -9,12 +11,48 @@ app.use(express.json());
 
 app.post("/signup", async (req, res) => {
   try {
-    const user = new User(req.body);
+    //validate the data
+
+    validateSignUpData(req);
+    const { firstName, lastName, emailId, password } = req.body;
+
+    //encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    //create new instance of the user model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
 
     await user.save();
     res.send("User added sucessfully");
   } catch (err) {
     res.status(400).send("Error saving the user :" + err.message);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    //email is available in db ?
+    const user = await User.findOne({ emailId: emailId });
+
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
+    // password check
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      res.send("Login successful");
+    } else {
+      throw new Error("Invalid credentials");
+    }
+  } catch (err) {
+    res.status(400).send("Error :" + err.message);
   }
 });
 
@@ -49,7 +87,7 @@ app.patch("/user/:userId", async (req, res) => {
   const userId = req.params?.userId;
   const data = req.body;
   try {
-    const ALLOWED_UPDATES = ["photourl", "about", "gender", "age", "skills"];
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
     const isUpdateAllowed = Object.keys(data).every((k) =>
       ALLOWED_UPDATES.includes(k)
     );
